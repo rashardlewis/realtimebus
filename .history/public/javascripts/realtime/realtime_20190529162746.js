d2ba7eb1cf0b8b0f4f17b3ce8dd6d4bb375e11db
wangshuai7 <wanghsuai7@100tal.com>
2019-05-29 16:47:40 +0800

    first submit

---
+++ .history/public/javascripts/realtime/realtime_20190529162746.js
@@ -0,0 +1,31 @@
+var express = require('express')
+var router = express.Router()
+import {
+    getBusList,
+    getDirectionFromBus,
+    getRealTimeInfo,
+    refresh
+} from '../public/javascripts/spiders/realtimebus'
+
+// 获取实时公交信息
+router.get('/bus/list', (req, res) => {
+    getBusList(req).then(response => {
+        res.send(response)
+    })
+})
+
+// 获取实时公交信息
+router.get('/bus/info', (req, res) => {
+    getRealTimeInfo(req).then(response => {
+        res.send(response)
+    })
+})
+
+// 刷新实时公交信息
+router.get('/bus/refresh', (req, res) => {
+    refresh(req).then(response => {
+        res.send(response)
+    })
+})
+
+module.exports = router
